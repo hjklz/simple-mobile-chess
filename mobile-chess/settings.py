33887d34673d4acad9f781abf25b62e652a315ca
Andy <aayao@ualberta.ca>
2018-12-17 14:56:15 -0700

    fixed order error

--- mobile-chess/settings.py
+++ mobile-chess/settings.py
@@ -13,9 +13,6 @@ import django_heroku
 import dj_database_url
 import os
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -87,6 +84,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
